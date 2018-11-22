86af1452a97ec90c390a88dbbccda31c6de8e577
Michael Irwin <mike.irwin1@gmail.com>
2018-11-22 14:49:03 +0000

    exercise 3 passed

--- friends.rb
+++ friends.rb
@@ -5,3 +5,10 @@ end
 def get_tv(person)
   return person[:favourites][:tv_show]
 end
+
+def likes_to_eat(person, food)
+  if person[:favourites][:snacks].include?(food)
+    return true
+  end
+  return false
+end
